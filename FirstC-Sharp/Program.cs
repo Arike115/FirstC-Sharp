809fc3afc8d2a99cfe4b7bf9dcdcc3883b023a51
Arike115 <ajibolaoluwafunke669@gmail.com>
2023-03-24 19:10:59 +0100

    Add project files.

---
+++ FirstC-Sharp/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using FirstC_Sharp;
+
+internal class Program //Class 
+{ //begining of statement
+    private static void Main(string[] args) //method //funtionality
+    {
+        Console.WriteLine("Hello, World!");
+        Student.AddStudent();
+
+
+
+
+
+
+    }
+
+}//end of a statement
---
+++ FirstC-Sharp/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstC_Sharp//container 1
+{
+    public static class Student //container 2
+    {
+        public static void AddStudent() //coantiner 3
+        {
+            Console.WriteLine("Welcome to your C# first class");
+
+
+
+
+
+
+        }
+
+
+    }
+}
