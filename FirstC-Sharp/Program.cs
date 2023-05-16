0895dcf26d50e61331559fa0d96c7086f08620e3
Arike115 <ajibolaoluwafunke669@gmail.com>
2023-05-16 17:13:00 +0100

    changes

--- FirstC-Sharp/Program.cs
+++ FirstC-Sharp/Program.cs
@@ -6,21 +6,49 @@ using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
-    //Attributes
-    //preprocessor directives
-    //xml documentation
-    
+    //LOOPS
+    //foreach
+    //for
+    //while
+    //dowhile
     static void Main(string[] args) //private method
     {
-        #region calculation method
-        var result = Logic.AddNum(7, 8);
-        Console.WriteLine(result);
-        #endregion
 
+        //foeach
+        List<string> words = new List<string> { "ben","joy","ruth","Todd","chalk"};
+        foreach (string word in words)
+        {
+            Console.WriteLine(word);
+        }
 
+        //for
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(i);
+        }
 
+        Console.WriteLine("while loop ........");
 
+        //while
 
+        int v = 0;
+        while (v < 10)
+        {
+            Console.WriteLine(v);
+            v++;
+
+        }
+
+        Console.WriteLine("do while ........");
+        //do while
+
+        int x = 0;
+        do
+        {
+            Console.WriteLine(x);
+            x++;
+        }
+        while (x < 10);
     }
 
 
