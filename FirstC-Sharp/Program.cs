28b12774b070a616902ff3d1131643d123110faa
Arike115 <ajibolaoluwafunke669@gmail.com>
2023-05-19 17:16:06 +0100

    changes

--- FirstC-Sharp/Program.cs
+++ FirstC-Sharp/Program.cs
@@ -1,64 +1,61 @@
 ﻿#define logic
 using FirstC_Sharp;
+using System.Collections;
 using System.Security.Cryptography.X509Certificates;
 
 
 
 class Program
 {
-    //LOOPS
-    //foreach
-    //for
-    //while
-    //dowhile
+    //List,Ilist,Icollection
+    //Queue
+    //stack
     static void Main(string[] args) //private method
     {
+        List<string> mylist = new List<string>();
+        mylist.Add("john");
+        mylist.Add("Ben"); 
+        mylist.Add("Tim"); 
+        mylist.Add("Joy"); 
+        mylist.Add("Esther");
 
-        //foeach
-        List<string> words = new List<string> { "ben","joy","ruth","Todd","chalk"};
-        foreach (string word in words)
-        {
-            Console.WriteLine(word);
-        }
+        MoreCollectionDisplay(mylist);
+        GenericDisplay(mylist);
+        Display(mylist);
 
-        //for
-        for (int i = 0; i < 5; i++)
-        {
-            Console.WriteLine(i);
-        }
 
-        Console.WriteLine("while loop ........");
 
-        //while
 
-        int v = 0;
-        while (v < 10)
-        {
-            Console.WriteLine(v);
-            v++;
 
-        }
 
-        Console.WriteLine("do while ........");
-        //do while
-
-        int x = 0;
-        do
-        {
-            Console.WriteLine(x);
-            x++;
-        }
-        while (x < 10);
     }
 
+    private static void Display(List<string> mylist)
+    {
+      
+        foreach (string item in mylist)
+        {
+            Console.WriteLine("The list item" + item);
+        }
+    }
 
+    private static void GenericDisplay(IList<string> mylist)
+    {
+      
+        foreach (string item in mylist)
+        {
+            Console.WriteLine("The ilist item" + item);
+        }
+    }
 
-
-
-
-
-
-
+    private static void MoreCollectionDisplay(ICollection<string> mylist)
+    {
+      
+       foreach(string item in mylist)
+        {
+            Console.WriteLine("The icollecttion list" + item);
+        }
+    }
 }
 
 
